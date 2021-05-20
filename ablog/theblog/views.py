3ed4752474b7e29a5ca3f3ea36537e64593d445d
Kamila <zhakupova.kamila@gmail.com>
2021-05-20 22:06:26 +0600

    Добавлены лайки, сетка на главной и загрузка картинок

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Category
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+
 
 # def home(request):
 #     return render(request, 'home.html', {})
+
+
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('article_detail', args=[str(pk)]))
 
 
 class HomeView(ListView):
@@ -18,6 +26,16 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data()
+
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context["cat_menu"] = cat_menu
+        context["total_likes"] = total_likes
+        return context
 
 
 class AddPostView(CreateView):
